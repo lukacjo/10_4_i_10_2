fc85405fb0962df181c3d7f9eb9caf16e80fd5a6
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-12-31 16:36:39 +0100

    aplikacja call_tmdb_api do wszystkich funkcji. usuniecie get_popular

--- tmdb_client.py
+++ tmdb_client.py
@@ -8,28 +8,6 @@ api_token = os.environ.get(
 )
 
 
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
-    headers = {"Authorization": f"Bearer {api_token}"}
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
-
-
-def get_poster_url(poster_api_path, size="w342"):
-    base_url = "https://image.tmdb.org/t/p/"
-    return f"{base_url}{size}/{poster_api_path}"
-
-
-"""def get_movies_list(list_type):
-    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
-    repsonse = requests.get(endpoint, headers=headers)
-    repsonse.raise_for_status()
-    return repsonse.json()"""
-
-
 def call_tmdb_api(
     endpoint,
 ):  # to pozwala nie powielaćtego całego tekstu w reszcie, tylko odpowiedni endpoint i dużo kodu mniej do pisania
@@ -38,6 +16,11 @@ def call_tmdb_api(
     response = requests.get(full_url, headers=headers)
     response.raise_for_status()
     return response.json()
+
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
 
 
 def get_movies_list(list_type):
@@ -51,28 +34,13 @@ def get_movies(how_many, list_type):
     return data[:how_many]
 
 
-"""def get_single_movie(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()"""
-
-
 def get_single_movie(movie_id):
     return call_tmdb_api(f"movie/{movie_id}")
 
 
 def get_single_movie_cast(movie_id, how_many):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {"Authorization": f"Bearer {api_token}"}
-    response = requests.get(endpoint, headers=headers)
-    return response.json()["cast"][:how_many]
+    return call_tmdb_api(f"movie/{movie_id}/credits")["cast"][:how_many]
 
 
 def get_movie_images(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    headers = {"Authorization": f"Bearer {api_token}"}
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return call_tmdb_api(f"movie/{movie_id}/images")
